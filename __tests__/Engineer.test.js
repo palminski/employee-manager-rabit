4f758b06b657a686b27588741c723378c4e32b36
palminski <williambolls98@gmail.com>
2022-09-12 12:12:09 -0700

    all classes added and all tests passed

---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,19 @@
+const Engineer = require("../lib/Engineer");
+
+test ('creates a new Engineer with proper values', () => {
+    const testEngineer = new Engineer("name","1234","email","github");
+
+    expect(testEngineer.name).toBe("name");
+    expect(testEngineer.id).toBe("1234");
+    expect(testEngineer.email).toBe("email");
+    expect(testEngineer.github).toBe("github");
+});
+
+test ('functions from Engineer can be called and return values', () => {
+    const testEngineer = new Engineer("name","1234","email","github");
+
+    expect(testEngineer.getName()).toEqual(testEngineer.name);
+    expect(testEngineer.getId()).toEqual(testEngineer.id);
+    expect(testEngineer.getEmail()).toEqual(testEngineer.email);
+    expect(testEngineer.getRole()).toEqual("Engineer");
+});
---
+++ __tests__/Intern.test.js
@@ -0,0 +1,20 @@
+const Intern = require("../lib/Intern");
+
+test ('creates a new Intern with proper values', () => {
+    const testIntern = new Intern("name","1234","email","school");
+
+    expect(testIntern.name).toBe("name");
+    expect(testIntern.id).toBe("1234");
+    expect(testIntern.email).toBe("email");
+    expect(testIntern.school).toBe("school");
+    
+});
+
+test ('functions from Intern can be called and return values', () => {
+    const testIntern = new Intern("name","1234","email","school");
+
+    expect(testIntern.getName()).toEqual(testIntern.name);
+    expect(testIntern.getId()).toEqual(testIntern.id);
+    expect(testIntern.getEmail()).toEqual(testIntern.email);
+    expect(testIntern.getRole()).toEqual("Intern");
+});
