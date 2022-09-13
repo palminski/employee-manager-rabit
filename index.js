b3be528290136ef11650e3c252d06f088734922b
palminski <williambolls98@gmail.com>
2022-09-13 10:58:11 -0700

    verify input

--- index.js
+++ index.js
@@ -16,22 +16,66 @@ const promptManager = () => {
         {
             type: "text",
             name: "name",
-            message: "Enter Manager's Name"
+            message: "Enter Manager's Name",
+            validate: input => {
+                if (input) {
+                    return true;
+                }
+                else
+                {
+                    console.log("please provide input!");
+                    return false;
+                }
+
+            }
         },
         {
             type: "text",
             name: "id",
-            message: "Enter Manager's Employee ID"
+            message: "Enter Manager's Employee ID",
+            validate: input => {
+                if (input) {
+                    return true;
+                }
+                else
+                {
+                    console.log("please provide input!");
+                    return false;
+                }
+
+            }
         },
         {
             type: "text",
             name: "email",
-            message: "Enter Manager's Email Adress"
+            message: "Enter Manager's Email Adress",
+            validate: input => {
+                if (input) {
+                    return true;
+                }
+                else
+                {
+                    console.log("please provide input!");
+                    return false;
+                }
+
+            }
         },
         {
             type: "text",
             name: "office",
-            message: "Enter Manager's office number"
+            message: "Enter Manager's office number",
+            validate: input => {
+                if (input) {
+                    return true;
+                }
+                else
+                {
+                    console.log("please provide input!");
+                    return false;
+                }
+
+            }
         },
         {
             type: "list",
@@ -64,22 +108,66 @@ const promptEngineer = () => {
         {
             type: "text",
             name: "name",
-            message: "Enter Engineer's Name"
+            message: "Enter Engineer's Name",
+            validate: input => {
+                if (input) {
+                    return true;
+                }
+                else
+                {
+                    console.log("please provide input!");
+                    return false;
+                }
+
+            }
         },
         {
             type: "text",
             name: "id",
-            message: "Enter Engineer's Employee ID"
+            message: "Enter Engineer's Employee ID",
+            validate: input => {
+                if (input) {
+                    return true;
+                }
+                else
+                {
+                    console.log("please provide input!");
+                    return false;
+                }
+
+            }
         },
         {
             type: "text",
             name: "email",
-            message: "Enter Engineer's Email Adress"
+            message: "Enter Engineer's Email Adress",
+            validate: input => {
+                if (input) {
+                    return true;
+                }
+                else
+                {
+                    console.log("please provide input!");
+                    return false;
+                }
+
+            }
         },
         {
             type: "text",
             name: "github",
-            message: "Enter Engineer's github"
+            message: "Enter Engineer's github",
+            validate: input => {
+                if (input) {
+                    return true;
+                }
+                else
+                {
+                    console.log("please provide input!");
+                    return false;
+                }
+
+            }
         },
         {
             type: "list",
@@ -112,22 +200,66 @@ const promptIntern = () => {
         {
             type: "text",
             name: "name",
-            message: "Enter Intern's Name"
+            message: "Enter Intern's Name",
+            validate: input => {
+                if (input) {
+                    return true;
+                }
+                else
+                {
+                    console.log("please provide input!");
+                    return false;
+                }
+
+            }
         },
         {
             type: "text",
             name: "id",
-            message: "Enter Intern's Employee ID"
+            message: "Enter Intern's Employee ID",
+            validate: input => {
+                if (input) {
+                    return true;
+                }
+                else
+                {
+                    console.log("please provide input!");
+                    return false;
+                }
+
+            }
         },
         {
             type: "text",
             name: "email",
-            message: "Enter Intern's Email Adress"
+            message: "Enter Intern's Email Adress",
+            validate: input => {
+                if (input) {
+                    return true;
+                }
+                else
+                {
+                    console.log("please provide input!");
+                    return false;
+                }
+
+            }
         },
         {
             type: "text",
             name: "school",
-            message: "Enter Intern's school"
+            message: "Enter Intern's school",
+            validate: input => {
+                if (input) {
+                    return true;
+                }
+                else
+                {
+                    console.log("please provide input!");
+                    return false;
+                }
+
+            }
         },
         {
             type: "list",
